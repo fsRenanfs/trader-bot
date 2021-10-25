191e502d4cbfa91a56926067df8fc8aa79179fa7
fsRenanfs <rferreirasantos98@gmail.com>
2021-10-25 15:23:29 -0300

    Add express to heroku test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ const BinanceWebSocketManager = require('./connection/binance_websocket_manager'
 const botConfig = require('../bot_conf.json');
 const config = require('../conf.json');
 
+//express para testes no heroku
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+//For avoidong Heroku $PORT error
+app
+  .get('/', function(request, response) {
+    var result = 'App is running';
+    response.send(result);
+  })
+  .listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+  });
+
+//--------------------------
+
 const ws = new BinanceWebSocketManager(
   config.binanceWebSocketUrl,
   botConfig.symbols,
